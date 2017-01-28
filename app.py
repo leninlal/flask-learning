48f7526ad756de2c970214b7dcdab573f3da7bb6
lenin <leninlalotp@gmail.com>
2017-01-28 10:47:10 +0530

    localization of date and time using flask_moment

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, request, make_response, abort, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 @app.route("/")
 def index():
-	return render_template('index.html', data=[1,2,3])
+	return render_template('index.html', data=[1,2,3], current_time=datetime.utcnow())
 
 @app.route("/user/<name>")
 def user(name):
